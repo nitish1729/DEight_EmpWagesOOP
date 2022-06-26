dccb5ff6f6af61b7f6fae2908af43a0c4bf1a5a1
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-26 12:05:14 +0530

    PartTimeWage

---
+++ PartTimeEmpWage.cs
@@ -0,0 +1,34 @@
+﻿
+
+public class PartTimeEmpWage
+{
+    int WagePerHrs = 20;
+    int FullTime = 8;
+    int NoWork = 0;
+
+    void display()
+    {
+       Random ran = new Random();
+
+       int AttenCheck = ran.Next(1, 10);
+
+        if (AttenCheck == FullTime)
+        {
+            Console.WriteLine("Emp is Present");
+            Console.WriteLine("EmpWage : "+(WagePerHrs * FullTime));
+        }
+        else
+        {
+            Console.WriteLine("Emp is Absent");
+            Console.WriteLine("EmpWage : " + (WagePerHrs * NoWork));
+        }
+    }
+    public static void Main(string[] args)
+    {
+        PartTimeEmpWage wages = new PartTimeEmpWage();
+
+        wages.display();
+    }
+}
+
+
