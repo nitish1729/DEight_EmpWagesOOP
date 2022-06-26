abd0992e4895758159526ae25dee1a57a13f40bf
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-26 11:32:45 +0530

    Welcome to OOP

---
+++ Welcome.cs
@@ -0,0 +1,14 @@
+﻿class Welcome
+{
+    void display()
+    {
+        Console.WriteLine("Welcome to Employee Wage Computation");
+
+    }
+    public static void Main()
+    {
+        Welcome Wel = new Welcome();
+        Wel.display();
+    }
+
+}
